9a42677a1874d25088bb5a386ea716793e8832fe
katger4 <kajgertz@gmail.com>
2017-04-12 22:58:49 -0700

    simplify dataframe creation/manipulation, add more comments

--- turbidity/turbidity.R
+++ turbidity/turbidity.R
@@ -1,47 +1,11 @@
 library(tidyverse)
-# library(ggmap)
 library(leaflet)
 library(MazamaSpatialUtils)
-
-# library(sp)  # classes for spatial data
-# library(raster)  # grids, rasters
-# library(rasterVis)  # raster visualisation
-# library(maptools)
-# library(rgeos)
 
 # NOTE: http://apps.leg.wa.gov/WAC/default.aspx?dispo=true&cite=173-201A-200
   #Turbidity shall not exceed:
   # 5 NTU over background when the background is 50 NTU or less; or
   # A 10 percent increase in turbidity when the background turbidity is more than 50 NTU.
-
-# load the turbidity data 
-turbidity <- read.csv('./data/EIMResults.csv', header = TRUE)
-
-# print out information on the variables it contains
-# summary(turbidity)
-
-# create a data frame tbl for easy printing
-# select only variables of interest, rename lengthy variable names
-# convert dates to date format
-# obtain log(x+1) since there are some negative values if use log(x)
-turb <- tbl_df(turbidity) %>%
-  select(Study_ID, Study_Name, Location_ID, Location_Name, 
-         Field_Collection_Start_Date, Field_Collection_End_Date,
-         Result_Value, Calculated_Latitude_Decimal_Degrees_NAD83HARN,
-          Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  rename(start_date = Field_Collection_Start_Date, end_date = Field_Collection_End_Date,
-         turbidity_NTU = Result_Value,
-         lat = Calculated_Latitude_Decimal_Degrees_NAD83HARN,
-         lon = Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  mutate(start_date = as.Date(start_date, format = "%m/%d/%Y"),
-         end_date = as.Date(end_date, format = "%m/%d/%Y"),
-         logturb = log10(turbidity_NTU+1))
-
-# dim(turb)
-# min(turb$end_date)
-# max(turb$end_date)
-# distinct(turb, Location_ID)
-# turb
 
 # load location data file
 # select only ID and WRIA
@@ -51,20 +15,22 @@ turbidity_locations <- read.csv('./data/EIMLocationDetails.csv', header = TRUE) 
 
 # create a turbidity data frame 
 # for merging with the tss data
-
+# select only variables of interest, rename lengthy variable names
+# convert dates to date format
+# obtain log(x+1) since there are some negative values if use log(x) (for plots)
 turbidity <- read.csv('./data/EIMResults.csv', header = TRUE) %>%
   select(Study_ID, Study_Name, Location_ID, 
          Field_Collection_Start_Date, Field_Collection_Start_Date_Time,
          Result_Value, Calculated_Latitude_Decimal_Degrees_NAD83HARN,
          Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  rename(start_date = Field_Collection_Start_Date, dt = Field_Collection_Start_Date_Time,
+  rename(start_date = Field_Collection_Start_Date, 
          measurement_value = Result_Value, 
          lat = Calculated_Latitude_Decimal_Degrees_NAD83HARN,
          lon = Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  # unite(PK, Study_ID, Location_ID, dt, remove = FALSE) %>%
   mutate(start_date = as.Date(start_date, format = "%m/%d/%Y"),
-         logValue = log10(measurement_value+1),
-         measurement_type = 'turbidity_NTU') %>%
+         logMeasurement = log10(measurement_value+1),
+         measurement_type = 'Turbidity',
+         unit = 'NTU') %>%
   # join wria to main data file
   # label wria name with wria number
   left_join(turbidity_locations, by = "Location_ID") %>% 
@@ -84,20 +50,19 @@ tss_locations <- read.csv('../TSS/data/EIMLocationDetails.csv', header = TRUE) %
 
 # load the tss data 
 # select only variables of interest, rename lengthy variable names
-# create Primary Key for merging with turbidity
 tss <- read.csv('../TSS/data/EIMResults.csv', header = TRUE) %>%
   select(Study_ID, Study_Name, Location_ID, 
          Field_Collection_Start_Date, Field_Collection_Start_Date_Time,
          Result_Value, Calculated_Latitude_Decimal_Degrees_NAD83HARN,
          Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  rename(start_date = Field_Collection_Start_Date, dt = Field_Collection_Start_Date_Time,
+  rename(start_date = Field_Collection_Start_Date,
          measurement_value = Result_Value,
          lat = Calculated_Latitude_Decimal_Degrees_NAD83HARN,
          lon = Calculated_Longitude_Decimal_Degrees_NAD83HARN) %>%
-  # unite(PK, Study_ID, Location_ID, dt, remove = FALSE) %>%
   mutate(start_date = as.Date(start_date, format = "%m/%d/%Y"),
-         logValue = log10(measurement_value+1),
-         measurement_type = 'tss_mgL') %>%
+         logMeasurement = log10(measurement_value+1),
+         measurement_type = 'TSS',
+         unit = 'mg/L') %>%
   left_join(tss_locations, by = "Location_ID") %>% 
   mutate(WRIA_ID = ifelse(Watershed_WRIA == "Kitsap", 15, 
                           ifelse(Watershed_WRIA == "Kennedy-Goldsborough", 14,
@@ -107,37 +72,59 @@ tss <- read.csv('../TSS/data/EIMResults.csv', header = TRUE) %>%
                                                       NA))))))
 
 
-# add both dataframes together, then clean up data
-TURB_TSS <- rbind(turbidity,tss)
+# add both dataframes together
+TURB_TSS <- rbind(turbidity,tss) 
 
-#update IDs
-df3$ID <- 1:nrow(df3)
+# add unique ID column
+TURB_TSS$ID <- 1:nrow(TURB_TSS)
 
-# # dim(turb2)
-# turb2 <- unique(turb2)
-# tss2 <- unique(tss2)
-# sm.tss2 <- select(tss2, PK, tss_mgL, logTSS)
-# 
-# # merge tss and turbidity (only rows with BOTH measures)
-# turb_tss <- turb2 %>%
-#   inner_join(sm.tss2, by = "PK") %>%
-#   left_join(locations, by = "Location_ID")
-# dim(turb_tss)
-# 
-# # merge tss and turbidity (all rows from turbidity measures)
-# TURB_tss <- turb2 %>%
-#   left_join(sm.tss2, by = "PK") %>%
-#   left_join(locations, by = "Location_ID")
-# dim(TURB_tss)
-# 
-# # merge tss and turbidity (ALL ROWS)
-# TURB_TSS <- turb2 %>%
-#   full_join(tss2, by = "PK") 
-# # %>%
-# #   left_join(locations, by = "Location_ID")
-# dim(TURB_TSS)
-# 
-# tssnas <- tss2[is.na(tss2$Location_ID),]
+# # then clean up data and 
+# TURB_TSS <- TURB_TSS %>%
+#   spread(measurement_type, measurement_value) %>%
+#   mutate(logTSS = log10(tss_mgL+1),
+#          logTurb = log10(turbidity_NTU+1))
+
+
+################################ HUCs ################################################ 
+# following introductory vignette at 
+# https://cran.r-project.org/web/packages/MazamaSpatialUtils/vignettes/introduction.html
+
+# need to install the data separately via command line (see mazama github)
+setSpatialDataDir('~/Data/Spatial')
+loadSpatialData('WBDHU')
+# only need to do the command below once (i think)
+# installSpatialData()
+
+# testing country stuff
+# loadSpatialData('NaturalEarthAdm1')
+# country <- tbl_df(getCountry(TURB_TSS$lon,TURB_TSS$lat, allData=TRUE))
+
+# get the HUC 12 and HUC 10 id's for each row
+huc_ids <- TURB_TSS %>%
+  mutate(HUC12_id = getHUC(lon, lat, SPDF = WBDHU12),
+         HUC10_id = getHUC(lon, lat, SPDF = WBDHU10))
+
+# get the HUC 12 Names for each HUC ID in the dataset
+huc12 <- tbl_df(getHUC(TURB_TSS$lon,TURB_TSS$lat, SPDF = WBDHU12, allData=TRUE)) %>%
+  # unite(coords, latitude, longitude, remove = FALSE) %>%
+  rename(HUC12_id = HUC, HUC12_Name = HUCName) %>%
+  select(HUC12_id, HUC12_Name)
+
+huc12 <- distinct(huc12)
+
+# get the HUC 10 Names for each HUC ID in the dataset
+huc10 <- tbl_df(getHUC(TURB_TSS$lon,TURB_TSS$lat, SPDF = WBDHU10, allData=TRUE)) %>%
+  rename(HUC10_id = HUC, HUC10_Name = HUCName) %>%
+  select(HUC10_id, HUC10_Name)
+
+huc10 <- distinct(huc10)
+
+# join the HUC Names to the HUC ids for each row
+water_huc <- huc_ids %>%
+  inner_join(huc12, by = 'HUC12_id') %>%
+  inner_join(huc10, by = 'HUC10_id')
+################################ HUCs ################################################ 
+
 
 ################################ mapping ################################################ 
 # turb_nums$loc=paste(turb_nums$lat, turb_nums$lon, sep=":") ## create a lat:long location variable
@@ -298,43 +285,3 @@ turb_tss %>%
   ggtitle("Turbidity vs TSS") +
   scale_x_date(date_breaks = '3 years', date_labels = '%Y')
 ################################ EDA plots ################################################ 
-
-################################ HUCs ################################################ 
-# following introductory vignette at 
-# https://cran.r-project.org/web/packages/MazamaSpatialUtils/vignettes/introduction.html
-
-# need to install the data separately via command line (see mazama github)
-setSpatialDataDir('~/Data/Spatial')
-loadSpatialData('WBDHU')
-# only need to do the command below once (i think)
-# installSpatialData()
-
-# testing country stuff
-# loadSpatialData('NaturalEarthAdm1')
-# country <- tbl_df(getCountry(TURB_TSS$lon,TURB_TSS$lat, allData=TRUE))
-
-# get the HUC 12 and HUC 10 id's for each row
-huc_ids <- TURB_TSS %>%
-  mutate(HUC12_id = getHUC(lon, lat, SPDF = WBDHU12),
-         HUC10_id = getHUC(lon, lat, SPDF = WBDHU10))
-
-# get the HUC 12 Names for each HUC ID in the dataset
-huc12 <- tbl_df(getHUC(TURB_TSS$lon,TURB_TSS$lat, SPDF = WBDHU12, allData=TRUE)) %>%
-  # unite(coords, latitude, longitude, remove = FALSE) %>%
-  rename(HUC12_id = HUC, HUC12_Name = HUCName) %>%
-  select(HUC12_id, HUC12_Name)
-
-huc12 <- distinct(huc12)
-
-# get the HUC 10 Names for each HUC ID in the dataset
-huc10 <- tbl_df(getHUC(TURB_TSS$lon,TURB_TSS$lat, SPDF = WBDHU10, allData=TRUE)) %>%
-  rename(HUC10_id = HUC, HUC10_Name = HUCName) %>%
-  select(HUC10_id, HUC10_Name)
-
-huc10 <- distinct(huc10)
-
-# join the HUC Names to the HUC ids for each row
-water_huc <- huc_ids %>%
-  inner_join(huc12, by = 'HUC12_id') %>%
-  inner_join(huc10, by = 'HUC10_id')
-################################ HUCs ################################################ 
