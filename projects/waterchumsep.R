d20516ee3e8e7b784bbf1a05fce30cea66cadffb
katger4 <kajgertz@gmail.com>
2017-04-25 21:44:30 -0700

    use jittered points for chum

--- projects/waterchumsep.R
+++ projects/waterchumsep.R
@@ -335,17 +335,17 @@ ch12 <- chum12 %>%
 # cohen's D: 'small effect' = 0.2, med effect = 0.5, large effect = 0.8
 
 ch12_formerge <- ch12 %>%
-  select(year:lat, HUC12_id, HUC12_Name, 
+  select(year:project_cat, jitterlng:jitterlat, HUC12_id, HUC12_Name, 
          medianyr, cohensd:colorblind) %>%
-  rename(description = sitename, measurement = count,
-         HUC_id = HUC12_id, HUC_Name = HUC12_Name) %>%
+  rename(description = sitename, lon = jitterlng, lat = jitterlat,
+         measurement = count, HUC_id = HUC12_id, HUC_Name = HUC12_Name) %>%
   mutate(result_type = 'Chum Salmon', unit = '', HUC_level = '12') 
 
 chum_formerge <- ch10 %>%
-  select(year:lat, Description, HUC10_id, HUC10_Name, 
+  select(year:project_cat, jitterlng:jitterlat, HUC10_id, HUC10_Name, 
          medianyr, cohensd:colorblind) %>%
-  rename(description = sitename, measurement = count,
-         HUC_id = HUC10_id, HUC_Name = HUC10_Name) %>%
+  rename(description = sitename, lon = jitterlng, lat = jitterlat,
+         measurement = count, HUC_id = HUC10_id, HUC_Name = HUC10_Name) %>%
   mutate(result_type = 'Chum Salmon', unit = '', HUC_level = '10') %>%
   rbind(ch12_formerge)
 
