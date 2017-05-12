5de1d933d4cc9271f5e8b8d76d30b8973bea8efb
katger4 <kajgertz@gmail.com>
2017-05-12 16:40:45 -0700

    add more chum site calculations to pipeline

--- projects/combine_data.R
+++ projects/combine_data.R
@@ -120,8 +120,8 @@ apply_cohensD <- function(data, outcome, HUC) {
       mutate(TimePeriod = ifelse(is.na(TimePeriod), 'noProject', TimePeriod)) %>%
       spread(TimePeriod, measurement) %>%
       mutate(cohensd_huc_mean = cohensD_manual(before, after),
-             mean_after = mean(after, na.rm = TRUE),
-             mean_before = mean(before, na.rm = TRUE)) %>%
+             huc_mean_after = mean(after, na.rm = TRUE),
+             huc_mean_before = mean(before, na.rm = TRUE)) %>%
       gather(key = 'TimePeriod', value = 'measurement', `before`, `after`, `during`, `noProject`, na.rm = TRUE) %>%
       mutate(effectsize = ifelse(abs(cohensd_huc_mean) < 0.5, 'small',
                                  ifelse(abs(cohensd_huc_mean) >= 0.8, 'large',
@@ -135,15 +135,26 @@ apply_cohensD <- function(data, outcome, HUC) {
       mutate(TimePeriod = ifelse(is.na(TimePeriod), 'noProject', TimePeriod)) %>%
       spread(TimePeriod, count) %>%
       mutate(cohensd = cohensD_manual(before, after),
+             site_mean_after = mean(after, na.rm = TRUE),
+             site_mean_before = mean(before, na.rm = TRUE),
+             site_sd_before = sd(before, na.rm = TRUE),
+             site_sd_after = sd(after, na.rm = TRUE),
+             var_pooled = sqrt((((length(after)-1)*(site_sd_after^2))+((length(before)-1)*(site_sd_before^2)))/(length(after)+length(before)-1)),
              var_cohensd = (length(before) + length(after))/(length(before) * length(after)) + (cohensd^2)/(2*(length(before) + length(after))),
-             wsubi = (1/var_cohensd),
+             sd_cohensd = var_cohensd^0.5,
+             wsubi = 1/var_cohensd,
              # correct_cd = -1*cohensd,
              wsubixd = cohensd*wsubi) %>%
       ungroup() %>%
       group_by_(HUC) %>%
       mutate(cohensd_huc_mean = (sum(wsubixd)/sum(wsubi)),
              huc_mean_after = mean(after, na.rm = TRUE),
-             huc_mean_before = mean(before, na.rm = TRUE)) %>%
+             huc_mean_before = mean(before, na.rm = TRUE),
+             sum_wsubixd = sum(wsubixd),
+             sum_wsubi = sum(wsubi),
+             cohensd_huc_var = 1/sum_wsubi,
+             cohensd_huc_sd = cohensd_huc_var^0.5,
+             plus_minus = cohensd_huc_sd*1.645) %>%
       gather(key = 'TimePeriod', value = 'count', `before`, `after`, `during`, `noProject`, na.rm = TRUE) %>%
       mutate(effectsize = ifelse(abs(cohensd_huc_mean) < 0.5, 'small',
                                  ifelse(abs(cohensd_huc_mean) >= 0.8, 'large',
@@ -250,6 +261,49 @@ chum10 <- chum_counts %>%
   left_join(huc10_med, by = "HUC10_Name") %>%
   apply_cohensD('chum', HUC = 'HUC10_Name') %>%
   add_status_colors('chum') 
+
+c10 <- chum_counts %>%
+  left_join(huc10_med, by = "HUC10_Name") %>%
+  group_by(site) %>%
+  mutate(TimePeriod = ifelse(year > medianyr, 'after',
+                             ifelse(year < medianyr, 'before',
+                                    'during'))) %>%
+  mutate(TimePeriod = ifelse(is.na(TimePeriod), 'noProject', TimePeriod)) %>%
+  spread(TimePeriod, count) %>%
+  filter('Duck' %in% site)
+  
+  summarize(cohensd = cohensD_manual(before, after),
+            nb = length(before),
+            na = length(after),
+         site_mean_after = mean(after, na.rm = TRUE),
+         site_mean_before = mean(before, na.rm = TRUE),
+         site_sd_before = sd(before, na.rm = TRUE),
+         site_sd_after = sd(after, na.rm = TRUE),
+         var_pooled = sqrt((((length(after)-1)*(site_sd_after^2))+((length(before)-1)*(site_sd_before^2)))/(length(after)+length(before)-1)),
+         var_cohensd = (length(before) + length(after))/(length(before) * length(after)) + (cohensd^2)/(2*(length(before) + length(after))),
+         sd_cohensd = var_cohensd^0.5,
+         wsubi = 1/var_cohensd,
+         # correct_cd = -1*cohensd,
+         wsubixd = cohensd*wsubi)
+
+
+    ungroup() %>%
+  group_by_(HUC) %>%
+  mutate(cohensd_huc_mean = (sum(wsubixd)/sum(wsubi)),
+         huc_mean_after = mean(after, na.rm = TRUE),
+         huc_mean_before = mean(before, na.rm = TRUE),
+         sum_wsubixd = sum(wsubixd),
+         sum_wsubi = sum(wsubi),
+         cohensd_huc_var = 1/sum_wsubi,
+         cohensd_huc_sd = cohensd_huc_var^0.5,
+         plus_minus = cohensd_huc_sd*1.645) %>%
+  gather(key = 'TimePeriod', value = 'count', `before`, `after`, `during`, `noProject`, na.rm = TRUE) %>%
+  mutate(effectsize = ifelse(abs(cohensd_huc_mean) < 0.5, 'small',
+                             ifelse(abs(cohensd_huc_mean) >= 0.8, 'large',
+                                    'medium')),
+         site_effectsize = ifelse(abs(cohensd) < 0.5, 'small',
+                                  ifelse(abs(cohensd) >= 0.8, 'large',
+                                         'medium')))
 
 ######################## END CHUM ########################
 
